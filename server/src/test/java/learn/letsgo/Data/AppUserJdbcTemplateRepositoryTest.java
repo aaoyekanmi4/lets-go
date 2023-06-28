61dae722a274639185a2ff234b45cf31d827de4c
Arit Oyekan <aaoyekanmi4@gmail.com>
2023-06-27 20:29:49 -0500

    AppUserJdbcTemplateRepository done

---
+++ server/src/test/java/learn/letsgo/Data/AppUserJdbcTemplateRepositoryTest.java
@@ -0,0 +1,20 @@
+package learn.letsgo.Data;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppUserJdbcTemplateRepositoryTest {
+
+    @Test
+    void findByUsername() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+}
