628e16525de1a0eb16b239293770e517be30bb27
Arit Oyekan <aaoyekanmi4@gmail.com>
2023-06-28 12:24:22 -0500

    completed jdbc test setup

--- server/src/main/java/learn/letsgo/Data/EventPostJdbcTemplateRepository.java
+++ server/src/main/java/learn/letsgo/Data/EventPostJdbcTemplateRepository.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 @Repository
 public class EventPostJdbcTemplateRepository implements EventPostRepository{
+
     @Override
     public EventPost findById(int postId) {
         return null;
--- server/src/test/java/learn/letsgo/Data/EventPostJdbcTemplateRepositoryTest.java
+++ server/src/test/java/learn/letsgo/Data/EventPostJdbcTemplateRepositoryTest.java
@@ -6,11 +6,13 @@ import learn.letsgo.Models.EventPost;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class EventPostJdbcTemplateRepositoryTest {
 
     @Autowired
