fd62bd766d03f1f943f5fa41be12effe384989ea
Arit Oyekan <aaoyekanmi4@gmail.com>
2023-06-30 19:49:01 -0500

    contact controller done

--- server/src/main/java/learn/letsgo/Security/SecurityConfig.java
+++ server/src/main/java/learn/letsgo/Security/SecurityConfig.java
@@ -43,6 +43,10 @@ public class SecurityConfig {
                 .antMatchers("/api/event/user/*").authenticated()
                 .antMatchers("/api/event/user/*/*").authenticated()
                 .antMatchers("/api/event/user/group/*/*/*").authenticated()
+                .antMatchers("/api/contact").authenticated()
+                .antMatchers("/api/contact/*").authenticated()
+                .antMatchers("/api/contact/user/*").authenticated()
+                .antMatchers("/api/contact/event/*").authenticated()
                 .antMatchers("/api/event/*").permitAll()
                 // if we get to this point, let's deny all requests
                 .antMatchers("/**").denyAll()
