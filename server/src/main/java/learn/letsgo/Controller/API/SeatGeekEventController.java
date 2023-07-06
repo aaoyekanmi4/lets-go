30ec9c4352ee6c81e6a4dcb391584ad46085f4cd
Arit Oyekan <aaoyekanmi4@gmail.com>
2023-07-05 22:45:48 -0500

    fixed update and delete event posts

--- server/src/main/java/learn/letsgo/Controller/API/SeatGeekEventController.java
+++ server/src/main/java/learn/letsgo/Controller/API/SeatGeekEventController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin
 public class SeatGeekEventController {
 
     private final SeatGeekEventService eventService;
--- server/src/main/java/learn/letsgo/Controller/API/TicketMasterEventController.java
+++ server/src/main/java/learn/letsgo/Controller/API/TicketMasterEventController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin
 public class TicketMasterEventController {
     private final TicketMasterEventService eventService;
 
